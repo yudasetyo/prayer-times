21d38c12f3e1e38383a33cf9cced6ef11ec13ba1
yudasetyo <yudasetyo07@gmail.com>
2024-01-28 14:12:14 +0700

    feat: fetch aladhan API

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+function prayerTimes(latitude, longitude) {
+  fetch(
+    "http://api.aladhan.com/v1/calendar?latitude=" +
+      latitude +
+      "&longitude=" +
+      longitude +
+      "&method=2"
+  )
+    .then((response) => response.json())
+    .then(function (response) {
+      let date = new Date();
+      let today = date.getDate() - 1;
+      //   console.log(response.data[today]);
+    });
+}
+
 function success(position) {
-  console.log(position);
+  prayerTimes(position.coords.latitude, position.coords.longitude);
 }
 
 function error() {
