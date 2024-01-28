1070ebca2a89c0c833d41e1e666265bc2f7e73cc
yudasetyo <yudasetyo07@gmail.com>
2024-01-28 13:54:26 +0700

    feat: add geolocation permission

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+function success(position) {
+  console.log(position);
+}
+
+function error() {
+  alert("Position not accessible!");
+}
+
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation is not supported in your browser!");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+
+  userLocation();
 }
 
-index(       );
+index();
